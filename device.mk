0acaae07c04821b87229ab1b5f825f533f7c23b3
alienabhishek <abhishekt295@gmail.com>
2018-11-22 06:29:59 +0530

    TOMATO:GPS AND FIXES

--- device.mk
+++ device.mk
@@ -61,7 +61,9 @@ PRODUCT_SYSTEM_PROPERTY_BLACKLIST := ro.product.model ro.sf.lcd_density
 
 # GPS
 PRODUCT_PACKAGES += \
-    gps.msm8916
+    gps.msm8916 \
+    libshims_get_process_name \
+    android.hardware.gnss@1.0-impl
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/gps/flp.conf:system/etc/flp.conf \
