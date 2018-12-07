dce19d4063b0faa9606c9ddd9704cc6fe8e0e708
alienabhishek <abhishekt295@gmail.com>
2018-12-07 08:42:50 +0530

    move setbaseband.sh to vendor

--- rootdir/etc/set_baseband.sh
+++ rootdir/etc/set_baseband.sh
@@ -1,4 +1,4 @@
-#!/system/bin/sh
+#!/system/vendor/bin/sh
 
 # grep the modem partition for baseband version and set it
 setprop gsm.version.baseband `strings /firmware/image/modem.b18 | grep "^MPSS.DPM" | head -1`
