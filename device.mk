b2533a85eb9ae876846181046226bf8992e997f8
alienabhishek <abhishekt295@gmail.com>
2018-12-12 16:28:39 +0530

    add lineage charger images

--- device.mk
+++ device.mk
@@ -56,6 +56,10 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     camera.device@1.0-impl
 
+PRODUCT_PACKAGES += \
+    lineage_charger_res_images \
+    font_log.png
+
 # Due to multi-density builds, these are set by init
 PRODUCT_SYSTEM_PROPERTY_BLACKLIST := ro.product.model ro.sf.lcd_density
 
